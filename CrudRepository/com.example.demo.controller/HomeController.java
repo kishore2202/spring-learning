b4523e6b01ebedfac887894bd54c7a91c2ffe25c
Kishore R <92428116+kishore2202@users.noreply.github.com>
2024-08-13 13:14:13 +0530

    Update HomeController.java

--- CrudRepository/com.example.demo.controller/HomeController.java
+++ CrudRepository/com.example.demo.controller/HomeController.java
@@ -2,16 +2,28 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
+import com.example.demo.service.UpdatedData;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 	
 	@Autowired
 	AlienRepo repo;
+	UpdatedData data;
+	
+	@Autowired
+	public HomeController(UpdatedData data) {
+        this.data = data;
+    }
 	
 	@RequestMapping("/")
 	public String home()
@@ -23,6 +35,31 @@ public class HomeController {
 	public String home(Alien alien)
 	{
 		repo.save(alien);
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/readAlien")
+	public ModelAndView readAlien(int aid, String aname)
+	{
+		ModelAndView mv=new ModelAndView("readAlien.jsp");
+		Alien alien=repo.findById(aid).orElse(new Alien());
+		mv.addObject(alien);
+		return mv;
+	}
+	
+	@RequestMapping("/deleteAlien")
+	public String deleteAlien(int aid)
+	{
+		repo.deleteById(aid);
+		return "home.jsp";
+	}
+	
+	@GetMapping("/update")
+	public String updateData(HttpServletRequest req, int aid)
+	{
+		String name=req.getParameter("name");
+		String lang=req.getParameter("lang");
+		data.updateData(aid, name, lang);
 		return "home.jsp";
 	}
 }
